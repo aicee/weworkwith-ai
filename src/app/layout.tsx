46954beb8f69e4c1acfc23c6d15afc8525e6fbb6
aicee <aiceetaguilaso@gmail.com>
2025-08-20 12:28:19 +0800

    add Vercel's speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -129,6 +130,7 @@ export default function RootLayout({
             <div className="flex-1">{children}</div>
           </div>
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
