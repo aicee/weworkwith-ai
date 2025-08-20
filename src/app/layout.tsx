1e5d8e5a480c8ac0b844bef502c446af03ef554b
aicee <aiceetaguilaso@gmail.com>
2025-08-20 12:30:12 +0800

    add Vercel's analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -131,7 +132,7 @@ export default function RootLayout({
           </div>
         </ThemeProvider>
         <SpeedInsights />
-      </body>
+        <Analytics />
     </html>
   );
 }
