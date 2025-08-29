97f51882416db987e696c0ee31e30769bd93640f
aicee <aiceetaguilaso@gmail.com>
2025-08-29 11:27:51 +0800

    update stats

--- src/data/statistics.tsx
+++ src/data/statistics.tsx
@@ -1,3 +1,3 @@
-export const totalCompanies = 12;
-export const newJobsThisWeek = 4;
-export const lastUpdated = "2025-08-27 16:04:18";
+export const totalCompanies = 13;
+export const newJobsThisWeek = 5;
+export const lastUpdated = "2025-08-29 11:14:18";
