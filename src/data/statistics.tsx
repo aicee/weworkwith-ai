1d5f87483fb2cb629c5bc908beb703553828c626
aicee <aiceetaguilaso@gmail.com>
2025-10-16 11:00:23 +0800

    updated job listings

--- src/data/statistics.tsx
+++ src/data/statistics.tsx
@@ -1,3 +1,3 @@
-export const totalCompanies = 15;
-export const newJobsThisWeek = 2;
-export const lastUpdated = "2025-09-04 09:22:18";
+export const totalCompanies = 3;
+export const newJobsThisWeek = 3;
+export const lastUpdated = "2025-10-16 11:00:18";
