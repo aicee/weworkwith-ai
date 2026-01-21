e685bbc17a17efab6d0b6983a98f746da782ac9b
aicee <aiceetaguilaso@gmail.com>
2026-01-21 18:16:51 +0800

    update a job and stats

--- src/data/statistics.tsx
+++ src/data/statistics.tsx
@@ -1,3 +1,3 @@
 export const totalCompanies = 3;
-export const newJobsThisWeek = 3;
-export const lastUpdated = "2025-10-16 11:00:18";
+export const newJobsThisWeek = 2;
+export const lastUpdated = "2026-01-21 18:00:18";
